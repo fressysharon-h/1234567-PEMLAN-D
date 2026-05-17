2f50b6b6b8fe49ae05edffb4b6750e5967199759
fressysharon-h <fressyshutabarat@gmail.com>
2026-05-17 11:33:19 +0700

    Create DataFutsal2a class with team data
    Initial implementation of DataFutsal2a class to manage team data.

---
+++ Modul 11/DataFutsal2a.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class DataFutsal2a {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> timA = new ArrayList<Integer>();
+        ArrayList<Integer> timB = new ArrayList<Integer>();
+
+        timA.add(168);
+        timA.add(170);
+        timA.add(165);
+        timA.add(168);
+        timA.add(172);
+        timA.add(170);
+        timA.add(169);
+        timA.add(165);
+        timA.add(171);
+        timA.add(166);
+
+        timB.add(170);
+        timB.add(167);
+        timB.add(165);
+        timB.add(166);
+        timB.add(168);
+        timB.add(175);
+        timB.add(172);
+        timB.add(171);
+        timB.add(168);
+        timB.add(169);
+
+        System.out.println("Data Tim A");
+        System.out.println(timA);
+
+        System.out.println("\nData Tim B");
+        System.out.println(timB);
+    }
+}
