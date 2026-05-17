f04a7c41be685ae461a322356a51c2d05544e221
fressysharon-h <fressyshutabarat@gmail.com>
2026-05-17 11:25:13 +0700

    1.a

---
+++ Modul 11/DataFutsal.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class DataFutsal {
+
+
+    public static void main(String[] args) {
+
+
+        Integer[] tinggiA = {168, 170, 165, 168, 172, 170, 169, 165, 171, 166};
+
+
+        Integer[] tinggiB = {170, 167, 165, 166, 168, 175, 172, 171, 168, 169};
+
+
+        System.out.println("=== TIM A ===");
+
+
+        Arrays.sort(tinggiA);
+        System.out.println("Ascending : " + Arrays.toString(tinggiA));
+
+
+        Arrays.sort(tinggiA, Collections.reverseOrder());
+        System.out.println("Descending: " + Arrays.toString(tinggiA));
+
+
+        System.out.println("\n=== TIM B ===");
+
+
+        Arrays.sort(tinggiB);
+        System.out.println("Ascending : " + Arrays.toString(tinggiB));
+
+
+        Arrays.sort(tinggiB, Collections.reverseOrder());
+        System.out.println("Descending: " + Arrays.toString(tinggiB));
+    }
+}
